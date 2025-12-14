1b0c957940e4a19d0895ab6a58783fb5bf919f83
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2025-12-14 20:35:48 +0530

    Create main script in Build override

--- jsdkman/exec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set Environment Variables
-export SDKMAN_DIR="$SNAP/_sdkman"
-export SDKMAN_CANDIDATES_DIR="$HOME/candidates"
-source "$SNAP/_sdkman/bin/sdkman-init.sh"
-# Run SDK
-sdk $@
