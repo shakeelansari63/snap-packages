c4a19f472c56e21f881defc82a0b9a43379d910e
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2025-12-15 19:20:49 +0530

    fix issues

--- jsdkman/exec.sh
+++ jsdkman/exec.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 export SDKMAN_DIR="$SNAP/_sdkman"
 export SDKMAN_CANDIDATES_DIR="$HOME/candidates"
+
+# Create some directories in home if they don't exist
+[[ -e "${HOME}/var" ]] || mkdir -p "${HOME}/var"
+[[ -e "${HOME}/candidates" ]] || mkdir -p "${HOME}/candidates"
+
+# Initialize SDKMAN Environment
 source "$SNAP/_sdkman/bin/sdkman-init.sh"
 sdk $@
