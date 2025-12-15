cbf123aa6159bd00c52e0ad94fb1d0e88f690c35
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2025-12-15 19:54:30 +0530

    Persist Data Dir

--- jsdkman/exec.sh
+++ jsdkman/exec.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export SDKMAN_DIR="$SNAP/_sdkman"
-export SDKMAN_CANDIDATES_DIR="$HOME/candidates"
+export SDKMAN_CANDIDATES_DIR="$SNAP_USER_COMMON/candidates"
 
 # Create some directories in home if they don't exist
-[[ -e "${HOME}/var" ]] || mkdir -p "${HOME}/var"
-[[ -e "${HOME}/candidates" ]] || mkdir -p "${HOME}/candidates"
+[[ -e "${SNAP_USER_COMMON}/tmp" ]] || mkdir -p "${SNAP_USER_COMMON}/tmp"
+[[ -e "${SNAP_USER_COMMON}/var" ]] || mkdir -p "${SNAP_USER_COMMON}/var"
+[[ -e "${SNAP_USER_COMMON}/candidates" ]] || mkdir -p "${SNAP_USER_COMMON}/candidates"
 
 # Initialize SDKMAN Environment
 source "$SNAP/_sdkman/bin/sdkman-init.sh"
