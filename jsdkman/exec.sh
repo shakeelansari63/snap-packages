77ead9598d9f35726a4023b15f82fe366ea2002a
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2025-12-15 20:47:16 +0530

    Fix Candidates Dir Path

--- jsdkman/exec.sh
+++ jsdkman/exec.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export SDKMAN_DIR="$SNAP/_sdkman"
-export SDKMAN_CANDIDATES_DIR="$SNAP_USER_COMMON/candidates"
 
 # Create some directories in home if they don't exist
 [[ -e "${SNAP_USER_COMMON}/tmp" ]] || mkdir -p "${SNAP_USER_COMMON}/tmp"
@@ -9,4 +8,9 @@ export SDKMAN_CANDIDATES_DIR="$SNAP_USER_COMMON/candidates"
 
 # Initialize SDKMAN Environment
 source "$SNAP/_sdkman/bin/sdkman-init.sh"
+
+# Reset Candidates Dir
+export SDKMAN_CANDIDATES_DIR="$SNAP_USER_COMMON/candidates"
+
+# Run SDK Command
 sdk $@
